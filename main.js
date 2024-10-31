6239c72bf958d40bb29dd26405659ec6e25d0787
Tien Tran Ba <trtien2803@gmail.com>
2024-10-31 11:10:05 +0700

    update loading

--- main.js
+++ main.js
@@ -1,36 +1,29 @@
-
-  function moveToSubmit(event) {
-    if (event.key === "Enter") {
-      checkLogin(); // Gọi hàm checkLogin() khi nhấn Enter
-    }
+function submitOnEnter(event) {
+  if (event.key === "Enter") {
+    checkLogin(); // Gọi hàm checkLogin() khi nhấn Enter
   }
-
+}
 
 function checkLogin() {
   var pass = document.getElementById('pass').value;
 
-  // Ẩn các phần tử khác
-  document.querySelector('.cloud-container').classList.add('hidden');
-  document.querySelector('.login').classList.add('hidden');
-
   if (pass === '1') {
-    // Hiển thị ảnh GIF 1
+    document.querySelector('.login').classList.add('hidden');
+    document.querySelector('.loading-container').style.display = 'flex';
     document.getElementById('successGif1').style.display = 'block';
-    
-    // Chuyển trang sau 3 giây
+
     setTimeout(function() {
       window.location.href = 'https://www.facebook.com/quangvy.van.1';
-    }, 3000);
+    }, 6000);
   } else if (pass === '2') {
-    // Hiển thị ảnh GIF 2
+    document.querySelector('.login').classList.add('hidden');
+    document.querySelector('.loading-container').style.display = 'flex';
     document.getElementById('successGif2').style.display = 'block';
-    
-    // Chuyển trang sau 3 giây
+
     setTimeout(function() {
-      window.location.href = 'https://www.youtube.com/'; // Đổi URL nếu cần
-    }, 3000);
+      window.location.href = 'https://www.youtube.com/';
+    }, 6000);
   } else {
     alert("Mật khẩu không đúng. Vui lòng thử lại!");
   }
 }
-  
